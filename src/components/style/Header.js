e3383ce15c3ef5ac73de2bd15142a9a9ec97b9c0
Elisa <elisangli11@gmail.com>
2021-06-11 19:25:35 -0400

    RVM-109 Update color

--- src/components/style/Header.js
+++ src/components/style/Header.js
@@ -2,10 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <nav
-      className="navbar navbar-expand-lg navbar-dark"
-      style={{ backgroundColor: "#6D597A" }}
-    >
+    <nav className="navbar navbar-expand-lg navbar-dark">
       <div className="container-fluid">
         <a className="navbar-brand" href="/">
           Review Me
@@ -24,24 +21,24 @@ const Header = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              <a id="head-link" className="nav-link" href="/">
                 Wish List
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              <a id="head-link" className="nav-link" href="/">
                 Library
               </a>
             </li>
           </ul>
           <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              <a id="head-link" className="nav-link" href="/">
                 Log In
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              <a id="head-link" className="nav-link" href="/">
                 Sign Up
               </a>
             </li>
