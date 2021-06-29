148a8bcd8879f5e3a548da94d2fe2a35af794b42
Sergey Kozyrev <skozyrev@myseneca.ca>
2021-06-29 13:42:14 -0400

    RVM-105 Create ContactUs page

--- frontend/src/components/contactus/ContactUs.js
+++ frontend/src/components/contactus/ContactUs.js
@@ -1,5 +1,7 @@
 import React from "react";
+
 import emailjs from "emailjs-com";
+import "./ContactUs.css";
 
 export default function ContactUs() {
   function sendEmail(e) {
@@ -23,15 +25,34 @@ export default function ContactUs() {
   }
 
   return (
-    <form className="contact-form" onSubmit={sendEmail}>
-      <input type="hidden" name="contact_number" />
-      <label>Name</label>
-      <input type="text" name="from_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <div className="card">
+      <div className="card-body">
+        <div className="mt-4 text-center">
+          <h1 className="card-title">Contact Us</h1>
+          <p className="card-subtitle">
+            Please include your name, email and message and we contact you as
+            soon as possible.
+          </p>
+        </div>
+        <form className="contact-form" onSubmit={sendEmail}>
+          <div className="mb-4">
+            <input type="hidden" name="contact_number" />
+            <label className="form-label">Name</label>
+            <input className="form-control" type="text" name="from_name" />
+          </div>
+          <div className="mb-4">
+            <label className="form-label">Email</label>
+            <input className="form-control" type="email" name="user_email" />
+          </div>
+          <label className="form-label">Message</label>
+          <div className="mb-4">
+            <textarea className="form-control" name="message" rows="6" />
+          </div>
+          <button className="button" type="submit">
+            Send
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
