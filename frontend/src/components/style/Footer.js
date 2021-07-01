498681551fdb7683a0f67a2190c223f7010cffa6
Elisa <elisangli11@gmail.com>
2021-06-30 22:02:00 -0400

    RVM-21 Add Logo

--- frontend/src/components/style/Footer.js
+++ frontend/src/components/style/Footer.js
@@ -2,18 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import "./Footer.css";
+import logo from "../../images/blue-logo.png";
 const Footer = () => {
   return (
     <footer className="footer mt-auto py-3 text-white">
       <div className="container">
         <div className="row">
-          <div className="col-5">
+          <div className="col-md-5">
             <Link className="text-white" id="footer-link" to="/terms">
               Terms & Conditions
             </Link>
           </div>
-          <div className="col-4">
-            <span> Review Me</span>
+          <div className="col-md-4">
+            <img src={logo} alt="Review Me" />
           </div>
           <div className="col">
             <Link className="text-white" id="footer-link" to="/about">
