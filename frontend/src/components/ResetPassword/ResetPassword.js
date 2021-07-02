45be889af60463564610c2e4b25e80b1980cb87a
Krupa Shah <shahkrupa1998@gmail.com>
2021-07-02 23:31:02 +0530

    RVM-34 Final Updated Reset Password

--- frontend/src/components/ResetPassword/ResetPassword.js
+++ frontend/src/components/ResetPassword/ResetPassword.js
@@ -29,64 +29,62 @@ const ResetPassword = () => {
     return (
         <div className="container light-style">
 
-            <h1 className="card-title">
-                <center>Account Settings</center>
-            </h1>
+            <div className="card">
 
-            <div className="card overflow-hidden">
+                <h1 className="card-title">
+                    <center>Account Settings</center>
+                </h1>
 
                 <div className="row no-gutters row-bordered row-border-light">
+
                     <div className="col-md-3 pt-0">
                         <div className="list-group list-group-flush account-settings-links ">
                             <Link className="list-group-item list-group-item-action" data-toggle="list" to="/profile">Profile</Link>
                             <Link className="list-group-item list-group-item-action active" data-toggle="list" to="/resetpassword">Reset Password</Link>
                         </div>
                     </div>
+
                     <div className="col-md-9">
                         <div className="tab-content">
 
                             <div className="tab-pane fade active show" id="resetpassword">
-
                                 <div className="card-body">
 
-                                    <div className="card-body pb-2">
+                                    <div className="form-group">
+                                        <label className="form-label">Current password</label>
+                                        <input type="password" className="form-control" />
+                                    </div>
 
-                                        <div className="form-group">
-                                            <label className="form-label">Current password</label>
-                                            <input type="password" className="form-control" />
+                                    <div className="form-group">
+                                        <div className={` ${validatePassword === false ? "invalid" : ""}`} >
+                                            <label className="form-label">New password</label>
+                                            <input type="password" className="form-control" onChange={passwordHandler} onBlur={validatePasswordHandler} />
+                                            {validatePassword === false ? <p>Password length should be greater than 8</p> : ""}
                                         </div>
+                                    </div>
 
-                                        <div className="form-group">
-                                            <div className={` ${validatePassword === false ? "invalid" : ""}`} >
-                                                <label className="form-label">New password</label>
-                                                <input type="password" className="form-control" onChange={passwordHandler} onBlur={validatePasswordHandler} />
-                                                {validatePassword === false ? <p>Password length should be greater than 8</p> : ""}
-                                            </div>
+                                    <div className="form-group">
+                                        <div className={` ${validateConfirmedPassword === false ? "invalid" : ""}`}>
+                                            <label className="form-label">Confirm password</label>
+                                            <input type="password" className="form-control" onChange={confirmedPasswordHandler} onBlur={validateConfirmedPasswordHandler} />
+                                            {validateConfirmedPassword === false ? <p>Passwords don't match. Re-enter the password</p> : ""}
                                         </div>
-
-                                        <div className="form-group">
-                                            <div className={` ${validateConfirmedPassword === false ? "invalid" : ""}`}>
-                                                <label className="form-label">Confirm password</label>
-                                                <input type="password" className="form-control" onChange={confirmedPasswordHandler} onBlur={validateConfirmedPasswordHandler} />
-                                                {validateConfirmedPassword === false ? <p>Passwords don't match. Re-enter the password</p> : ""}
-                                            </div>
-                                        </div>
-
                                     </div>
 
                                 </div>
 
                                 <div className="text-center mt-3">
-                                    <button type="button" className="button mb-4">Save changes</button>&nbsp;
-                                    <button type="button" className="button mb-4">Cancel</button>
+                                    <button type="button" className="button mb-4">Save changes</button>
                                 </div>
                             </div>
 
                         </div>
                     </div>
+
                 </div>
 
             </div>
+
         </div>
     )
 }
