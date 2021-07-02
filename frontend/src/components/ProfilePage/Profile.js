177d6dd37347909691e1d091c5893b18804b1916
Krupa Shah <shahkrupa1998@gmail.com>
2021-07-02 13:44:07 +0530

    RVM-32 Profile Page final commit

--- frontend/src/components/ProfilePage/Profile.js
+++ frontend/src/components/ProfilePage/Profile.js
@@ -5,14 +5,44 @@ import { useState } from 'react';
 
 const Profile = () => {
 
+    const dobExpression = /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/;
+    const emailExpression = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
     const [firstName, setFirstName] = useState("John");
     const [lastName, setLastName] = useState("Doe");
     const [nickname, setNickname] = useState("JohnDoe12");
     const [gender, setGender] = useState("Male");
-    const [dob, setDob] = useState("1999/11/11");
+    const [dob, setDob] = useState("11/13/1999");
     const [email, setEmail] = useState("johndoe@mail.com");
 
+    const [validateFirstName, setValidateFirstName] = useState();
+    const [validateLastName, setValidateLastName] = useState();
+    const [validateNickname, setValidateNickname] = useState();
+    const [validateDob, setValidateDob] = useState();
+    const [validateEmail, setValidateEmail] = useState();
+
     const options = ["Male", "Female", "Other"];
+
+    const validatedFirstNameHandler = () => {
+        setValidateFirstName(firstName.trim().length > 0 ? true : false);
+    }
+
+    const validatedLastNameHandler = () => {
+        setValidateLastName(lastName.trim().length > 0 ? true : false);
+    }
+
+    const validateNicknameHandler = () => {
+        setValidateNickname(nickname.trim().length > 0 ? true : false);
+    }
+
+    const validateDobHandler = () => {
+        setValidateDob(dob.match(dobExpression) ? true : false);
+    }
+
+    const validateEmailHandler = () => {
+        setValidateEmail(email.match(emailExpression) ? true : false);
+    }
+
 
     return (
         <div className="container light-style">
@@ -40,28 +70,37 @@ const Profile = () => {
 
                                         <div className="col-md-6">
                                             <div className="form-group">
-                                                <label className="form-label">First Name</label>
-                                                <input type="text" className="form-control" value={firstName} onChange={(event) => {
-                                                    setFirstName(event.target.value);
-                                                }} />
+                                                <div className={`${validateFirstName === false ? "invalid" : ""}`}>
+                                                    <label className="form-label">First Name</label>
+                                                    <input type="text" className="form-control" value={firstName} onChange={(event) => {
+                                                        setFirstName(event.target.value);
+                                                    }} onBlur={validatedFirstNameHandler} />
+                                                    {validateFirstName === false ? <p>Please enter your First Name</p> : ""}
+                                                </div>
                                             </div>
                                         </div>
 
                                         <div className="col-md-6">
                                             <div className="form-group">
-                                                <label className="form-label">Last Name</label>
-                                                <input type="text" className="form-control" value={lastName} onChange={(event) => {
-                                                    setLastName(event.target.value);
-                                                }} />
+                                                <div className={`${validateLastName === false ? "invalid" : ""}`}>
+                                                    <label className="form-label">Last Name</label>
+                                                    <input type="text" className="form-control" value={lastName} onChange={(event) => {
+                                                        setLastName(event.target.value);
+                                                    }} onBlur={validatedLastNameHandler} />
+                                                    {validateLastName === false ? <p>Please enter your Last Name</p> : ""}
+                                                </div>
                                             </div>
                                         </div>
 
                                         <div className="col-md-6">
                                             <div className="form-group">
-                                                <label className="form-label">Nickname</label>
-                                                <input type="text" className="form-control" value={nickname} onChange={(event) => {
-                                                    setNickname(event.target.value);
-                                                }} />
+                                                <div className={`${validateNickname === false ? "invalid" : ""}`}>
+                                                    <label className="form-label">Nickname</label>
+                                                    <input type="text" className="form-control" value={nickname} onChange={(event) => {
+                                                        setNickname(event.target.value);
+                                                    }} onBlur={validateNicknameHandler} />
+                                                    {validateNickname === false ? <p>Please enter your Nickname</p> : ""}
+                                                </div>
                                             </div>
                                         </div>
 
@@ -74,24 +113,29 @@ const Profile = () => {
                                                 <option> {options[1]} </option>
                                                 <option> {options[2]} </option>
                                             </select>
-
                                         </div>
 
                                         <div className="col-md-6">
                                             <div className="form-group">
-                                                <label className="form-label">Date of Birth</label>
-                                                <input type="text" className="form-control" value={dob} onChange={(event) => {
-                                                    setDob(event.target.value);
-                                                }} />
+                                                <div className={`${validateDob === false ? "invalid" : ""}`}>
+                                                    <label className="form-label">Date of Birth</label>
+                                                    <input type="text" className="form-control" value={dob} onChange={(event) => {
+                                                        setDob(event.target.value);
+                                                    }} onBlur={validateDobHandler} />
+                                                    {validateDob === false ? <p>Please enter DOB in MM/DD/YYYY format</p> : ""}
+                                                </div>
                                             </div>
                                         </div>
 
                                         <div className="col-md-6">
                                             <div className="form-group">
-                                                <label className="form-label">E-mail</label>
-                                                <input type="email" className="form-control" value={email} onChange={(event) => {
-                                                    setEmail(event.target.value);
-                                                }} />
+                                                <div className={`${validateEmail === false ? "invalid" : ""}`}>
+                                                    <label className="form-label">E-mail</label>
+                                                    <input type="email" className="form-control" value={email} onChange={(event) => {
+                                                        setEmail(event.target.value);
+                                                    }} onBlur={validateEmailHandler} />
+                                                    {validateEmail === false ? <p>Please enter Email in proper format</p> : ""}
+                                                </div>
                                             </div>
                                         </div>
 
