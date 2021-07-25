66a3671e6c72658f971aab37a5d31f76c76aa8cd
Krupa Shah <shahkrupa1998@gmail.com>
2021-07-26 01:22:30 +0530

    RVM-151 Profile Backend

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -3,5 +3,6 @@ const controller = {};
 controller.review = require("./review.controller");
 controller.library = require("./library.controller");
 controller.wishlist = require("./wishlist.controller");
+controller.profile = require("./profile.controller");
 
 module.exports = controller;
