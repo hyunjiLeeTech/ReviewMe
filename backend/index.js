cd540252f3e66505cf8206d8ca1b3fdeb9cc66a8
Sergey Kozyrev <skozyrev@myseneca.ca>
2021-08-03 10:49:39 -0400

    loading animation added on new version of main branch + added more user feedback on signup (#79)
    * loading animation added on new version of main branch
    
    * added more user feedback on signup
    
    * minor update
    
    * home route changed for admin user
    
    * Resolve sign up issue
    
    * Make it empty after submit sign up
    
    Co-authored-by: HyunjiLee <hyunji.lee.tech@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
+//#region User
 app.get("/users", (req, res) => {
   controllers.users.getUsers().then((data) => {
     console.log(data.rows.userid);
@@ -60,59 +61,40 @@ app.post("/auth/login", async (req, res) => {
 });
 
 app.post("/auth/signup", async (req, res) => {
-  // controllers.users
-  //   .login(req)
-  //   .then((data) => {})
-  //   .catch((err) => {
-  //     res.status(401).json();
-  //   });
-  try {
-    let gender_type;
-    const {
-      nickName,
-      email,
-      password,
-      password2,
-      firstName,
-      lastName,
-      gender,
-      dob,
-    } = req.body;
-    if (gender === "Male") {
-      gender_type = 1;
-    } else if (gender === "Female") {
-      gender_type = 2;
-    } else if (gender === "Prefer not to say") {
-      gender_type = 3;
-    }
+  const {
+    nickName,
+    email,
+    password,
+    password2,
+    firstName,
+    lastName,
+    gender,
+    dob,
+  } = req.body;
 
-    const user = await sequelize.query(
-      `select userid from usr where email='${email}'`
-    );
-    console.log(user);
-    const userNickname = await sequelize.query(
-      `select nickname from userdetails where nickname='${nickName}'`
-    );
-    if (user[0] != "" || userNickname[0] != "") {
-      res.json("User already exist");
-    } else {
-      const saltRound = 10;
-      const salt = await bcrypt.genSalt(saltRound);
+  const userInfo = {
+    nickName: nickName,
+    email: email,
+    password: password,
+    password2: password2,
+    firstName: firstName,
+    lastName: lastName,
+    gender: gender,
+    dob: dob,
+  };
 
-      const bcryptPassword = await bcrypt.hash(password, salt);
-      const newUser = await sequelize.query(
-        `insert into usr (userid, email, password, usertypeid, isactive) values((select max(userid)+1 from usr),'${email}', '${bcryptPassword}', 2, true) returning *`
-      );
-      sequelize.query(
-        `insert into userdetails(userdetailid, firstname, lastname, nickname, dateofbirth, genderid, userid) values((select max(userdetailid)+1 from userdetails),
-         '${firstName}', '${lastName}', '${nickName}','${dob}','${gender_type}', (select max(userid) from usr))`
-      );
-      res.json("User successfully created");
-    }
-  } catch (err) {
-    console.error(err.message);
-  }
+  controllers.users
+    .login(req)
+    .then((data) => {})
+    .catch((err) => {
+      res.status(401).json();
+    });
+
+  controllers.users.signup(userInfo).then((result) => {
+    res.json(result);
+  });
 });
+//#endregion
 
 //#region Reviews
 app.get("/reviews", (req, res) => {
---
+++ frontend/src/components/style/BookLoading.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./BookLoading.css";
+
+const BookLoading = () => {
+  return (
+    <div class="book">
+      <span class="page turn"></span>
+      <span class="page turn"></span>
+      <span class="page turn"></span>
+      <span class="page turn"></span>
+      <span class="page turn"></span>
+      <span class="page turn"></span>
+      <span class="cover"></span>
+      <span class="page"></span>
+      <span class="cover turn"></span>
+    </div>
+  );
+};
+
+export default BookLoading;
