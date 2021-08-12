26acebf3f5e104d83475f5603bbc586500fd228a
HyunjiLee <hyunji.lee.tech@gmail.com>
2021-08-12 14:10:50 -0400

    RVM-187 Display precision 1 not 0.5

--- frontend/src/components/BookDetails/BookDetails.js
+++ frontend/src/components/BookDetails/BookDetails.js
@@ -196,7 +196,7 @@ const BookDetails = (props) => {
                 onClick={handleRatingChange}
                 name="rating"
                 value={rating}
-                precision={0.5}
+                precision={1}
               />
               <textarea
                 placeholder="You have a comment ? *"
