e3199ca469b7631eeeb0c2f654aef2707edd0e7b
HyunjiLee <hyunji.lee.tech@gmail.com>
2021-08-13 18:25:13 -0400

    Update contact us message

--- frontend/src/components/AboutUs/AboutUs.js
+++ frontend/src/components/AboutUs/AboutUs.js
@@ -10,25 +10,25 @@ const AboutUs = () => {
     {
       name: "Elisa Ng Li",
       content:
-        "Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus inmassa egestas mollis varius; dignissim elementum. Mollistincidunt mattis hendrerit dolor eros enim, nisi ligula ornare.Hendrerit parturient habitant pharetra rutrum gravida porttitoreros feugiat. Mollis elit sodales taciti duis praesent id.Consequat urna vitae morbi nunc congue.",
+        "Hello, This is Elisa! I am one of the developers of this application. I was in charge of some of the frontend and backend code. Also, I was the team leader but everybody help and contribute to the project.\nFun Facts:\nWhen I am not coding, I usually spend my time sleeping and watching movies!",
       image: `${process.env.PUBLIC_URL}/images/bee.png`,
     },
     {
       name: "Hyun Ji Lee",
       content:
-        "Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus inmassa egestas mollis varius; dignissim elementum. Mollistincidunt mattis hendrerit dolor eros enim, nisi ligula ornare.Hendrerit parturient habitant pharetra rutrum gravida porttitoreros feugiat. Mollis elit sodales taciti duis praesent id.Consequat urna vitae morbi nunc congue.",
+        "Hi! I'm Jane (Hyun Ji) Lee. I worked as a lead developer on this team. We helped each other a lot to complete this project. I had lots of fun while we were working as a team! I participated in setups, implementing frontend and backend. In my free time, I love watching some videos and reading books!",
       image: `${process.env.PUBLIC_URL}/images/koala.png`,
     },
     {
       name: "Krupa Kirtikumar Shah",
       content:
-        "Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus inmassa egestas mollis varius; dignissim elementum. Mollistincidunt mattis hendrerit dolor eros enim, nisi ligula ornare.Hendrerit parturient habitant pharetra rutrum gravida porttitoreros feugiat. Mollis elit sodales taciti duis praesent id.Consequat urna vitae morbi nunc congue.",
+        "Hello, I am Krupa. I contributed to the development of this application. I hope you find it useful. If you need to reach out to any of us just contact us.\nFun Facts:\nI can not function without coffee. In my free time, I just binge-watch Netflix shows and sleep.",
       image: `${process.env.PUBLIC_URL}/images/rabbit.png`,
     },
     {
       name: "Sergey Kozyrev",
       content:
-        "Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus inmassa egestas mollis varius; dignissim elementum. Mollistincidunt mattis hendrerit dolor eros enim, nisi ligula ornare.Hendrerit parturient habitant pharetra rutrum gravida porttitoreros feugiat. Mollis elit sodales taciti duis praesent id.Consequat urna vitae morbi nunc congue.",
+        "Hello, my name is Sergey. I am a developer who likes to do frontend most of the time than work with the backend, but always accepts challenges. Besides coding, I like to spend time watching some movies, shows and eat sweets.",
       image: `${process.env.PUBLIC_URL}/images/toucan.png`,
     },
   ];
