66a72d0b4bd108e0e7cb93ba88ed5b0582a55c22
Krupa Shah <shahkrupa1998@gmail.com>
2021-08-13 15:05:35 +0530

    RVM-150 Final Commit

--- frontend/src/components/ReportManager/ReportManager.js
+++ frontend/src/components/ReportManager/ReportManager.js
@@ -16,19 +16,34 @@ let PageSize = 8;
 
 const ReportManager = () => {
 
-  const [reports, setReports] = useState([]);
+  const [allReports, setAllReports] = useState([]);
+  const [filteredReports, setFilteredReports] = useState([]);
 
   useEffect(() => {
     ReportDataService.getAllReports().then((reportData) => {
-      setReports(reportData);
+      setAllReports(reportData);
+      setFilteredReports(reportData);
     });
   }, []);
+
+  const filterFunction = (reportType) => {
+    if (reportType === "All") {
+      setFilteredReports(allReports);
+    }
+    else {
+      const dummyData = allReports.filter((item) => {
+        return item.reporttype === reportType;
+      })
+      setFilteredReports(dummyData);
+    }
+  }
 
   const onDelete = (reviewId, reportId) => {
     ReportDataService.deleteReport(reportId, reviewId).then((data) => {
       if (data === true) {
         ReportDataService.getAllReports().then((reportData) => {
-          setReports(reportData);
+          setAllReports(reportData);
+          setFilteredReports(reportData);
         });
       }
     })
@@ -38,7 +53,8 @@ const ReportManager = () => {
     ReportDataService.keepReport(reportId).then((data) => {
       if (data === true) {
         ReportDataService.getAllReports().then((reportData) => {
-          setReports(reportData);
+          setAllReports(reportData);
+          setFilteredReports(reportData);
         });
       }
     })
@@ -46,11 +62,11 @@ const ReportManager = () => {
 
   const [currentPage, setCurrentPage] = useState(1);
 
-  const reportData = useMemo(() => {
+  const filteredReportsData = useMemo(() => {
     const firstPageIndex = (currentPage - 1) * PageSize;
     const lastPageIndex = firstPageIndex + PageSize;
-    return reports.slice(firstPageIndex, lastPageIndex);
-  }, [currentPage, reports]);
+    return filteredReports.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage, filteredReports]);
 
   return (
     <div className="container-fluid">
@@ -65,19 +81,19 @@ const ReportManager = () => {
               id="menu"
             >
               <li className="nav-item mt-2">
-                <Link to="report-admin" className="nav-link align-middle px-0">
+                <Link to="report-admin" className="nav-link align-middle px-0" onClick={() => filterFunction("All")}>
                   <MessageIcon />
                   <span className="ms-1 d-none d-sm-inline">All</span>
                 </Link>
               </li>
               <li className="nav-item mt-2">
-                <a href="#spoiler" className="nav-link align-middle px-0">
+                <a href="#spoiler" className="nav-link align-middle px-0" onClick={() => filterFunction("Spoilers")}>
                   <AnnouncementOutlinedIcon />
                   <span className="ms-1 d-none d-sm-inline">Spoiler</span>
                 </a>
               </li>
               <li className="nav-item mt-2">
-                <a href="#hateSpeech" className="nav-link px-0 align-middle">
+                <a href="#hateSpeech" className="nav-link px-0 align-middle" onClick={() => filterFunction("Hate Speech")}>
                   <SpeakerNotesOffOutlinedIcon />
                   <span className="ms-1 d-none d-sm-inline">Hate Speech</span>
                 </a>
@@ -87,16 +103,15 @@ const ReportManager = () => {
           </div>
         </div>
         <div className="col py-3">
+
           <div className="text-center mt-3">
             <Title name="Reports" />
           </div>
+
           <div className="row d-flex justify-content-center">
             <div className="col-md-8">
-              <div id="spoiler" className="cards">
-                {reportData
-                  .filter((item) => {
-                    return item.reporttype === "Spoilers";
-                  })
+              <div className="cards">
+                {filteredReportsData
                   .map((data, index) => (
                     <ReportComment
                       key={index.toString()}
@@ -111,34 +126,17 @@ const ReportManager = () => {
                     />
                   ))}
               </div>
-              <div id="hateSpeech" className="cards">
-                {reportData
-                  .filter((item) => {
-                    return item.reporttype === "Hate Speech";
-                  })
-                  .map((data, index) => (
-                    <ReportComment
-                      key={index.toString()}
-                      reporterName={data.reporterName}
-                      reporterComment={data.reporterComment}
-                      reviewerName={data.reviewerName}
-                      reviewerComment={data.reporterComment}
-                      reviewId={data.reviewid}
-                      reportId={data.reportid}
-                      onDelete={onDelete}
-                      onKeep={onKeep}
-                    />
-                  ))}
-              </div>
+
               <div className="d-flex justify-content-center">
                 <Pagination
                   className="pagination-bar"
                   currentPage={currentPage}
-                  totalCount={reports.length}
+                  totalCount={filteredReports.length}
                   pageSize={PageSize}
                   onPageChange={(page) => setCurrentPage(page)}
                 />
               </div>
+
             </div>
           </div>
         </div>
